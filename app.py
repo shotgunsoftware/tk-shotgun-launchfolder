a5c35e42bba54e67cfec6ba5d955cf6ac773e2b2
Philip Scadding <pscadding@users.noreply.github.com>
2018-11-06 14:12:18 +0000

    SG-6302 better path handling (#1)
    Bug Fix and Small Feature added
    
    - Now syncs path cache before trying to find context folders.
    - If the entity type the action is being run against is a `PublishedFile` entity, it will try to get the folder path from the entity's `path` field, and open the folder at that location, highlighting the file in the window (except on Linux or symbolic file paths)

--- app.py
+++ app.py
@@ -12,14 +12,16 @@
 App that launches a folder browser from inside of Shotgun
 """
 
-import tank
+import sgtk
+from sgtk.util.errors import PublishPathNotDefinedError, PublishPathNotSupported
+from sgtk.util import filesystem
 import sys
 import os
+import subprocess
 
-class LaunchFolder(tank.platform.Application):
+class LaunchFolder(sgtk.platform.Application):
     
     def init_app(self):
-        entity_types = self.get_setting("entity_types")
         deny_permissions = self.get_setting("deny_permissions")
         deny_platforms = self.get_setting("deny_platforms")
         
@@ -31,46 +33,73 @@ class LaunchFolder(tank.platform.Application):
         }
         
         self.engine.register_command("show_in_filesystem", self.show_in_filesystem, p)
-            
-    def launch(self, path):
-        self.log_debug("Launching file system viewer for folder %s" % path)        
-        
-        # get the setting        
-        system = sys.platform
-        
-        # run the app
-        if system == "linux2":
-            cmd = 'xdg-open "%s"' % path
-        elif system == "darwin":
-            cmd = 'open "%s"' % path
-        elif system == "win32":
-            cmd = 'cmd.exe /C start "Folder" "%s"' % path
-        else:
-            raise Exception("Platform '%s' is not supported." % system)
-        
-        self.log_debug("Executing command '%s'" % cmd)
-        exit_code = os.system(cmd)
-        if exit_code != 0:
-            self.log_error("Failed to launch '%s'!" % cmd)
 
-    
+    def _get_published_file_path(self, entity_id):
+        """
+        Will attempt to return back a resolved path for a PublishedFile entity
+        :param entity_id: int for the PublishedFile ID.
+        :return: str path or None depending on whether it successfully resolves the path.
+        """
+
+        # Find the publish so that we can get the path back.
+        # Note there is a bug with the resolve_publish_path method requiring the `code` to be present, even though we
+        # don't really need it. If this bug (SG-8561) is fixed in the future we can remove the need to gather the code.
+        published_file = self.sgtk.shotgun.find_one("PublishedFile", [["id", "is", entity_id]], ["code", "path"])
+        # Resolve the path for the local OS
+        try:
+            local_path = sgtk.util.resolve_publish_path(self.sgtk, published_file)
+            self.log_debug("Gathered path for PublishedFile entity %s path: %s " % (entity_id, local_path))
+        except (PublishPathNotDefinedError, PublishPathNotSupported) as e:
+            # It might fail to resolve the path to the publish if the published file is an uploaded file
+            # or URL, in which case we revert to the default context based method.
+            self.log_warning("Publish path couldn't be resolved "
+                             "falling back to context based path; reason: %s" % e)
+            local_path = None
+
+        return local_path
+
     def show_in_filesystem(self, entity_type, entity_ids):
         """
         Pop up a filesystem finder window for each folder associated
         with the given entity ids.
         """
         paths = []
-        
+
+        # As the this method relies on the path_cache to find the folders,
+        # we must check we are in sync before continuing.
+        self.sgtk.synchronize_filesystem_structure()
+
         for eid in entity_ids:
-            # Use the path cache to look up all paths linked to the task's entity
-            context = self.tank.context_from_entity(entity_type, eid)
-            paths.extend( context.filesystem_locations )
+
+            pub_file_path = None
+
+            if entity_type == "PublishedFile":
+                # If the entity type is a PublishedFile, we should try at first to extract the path of the publish
+                # and open the folder for that. If we can't we will fall back on the context driven path.
+                self.log_debug("Getting path for PublishedFile entity %s " % eid)
+                pub_file_path = self._get_published_file_path(eid)
+
+            if pub_file_path:
+                paths.append(pub_file_path)
+
+            else:
+                # We've not found a path to a specific file for the entity, so we should try to resolve the path
+                # using the path cache instead.
+
+                # Use the path cache to look up all paths linked to the task's entity
+                context = self.sgtk.context_from_entity(entity_type, eid)
+
+                # todo: Add support for step folders, when a step is present in the context, open the step folder
+
+                # We don't have a PublishedFile path, so we should just use the standard context paths
+                # associated with this entity in the path cache.
+                paths.extend(context.filesystem_locations)
                             
         if len(paths) == 0:
             self.log_info("No location exists on disk yet for any of the selected entities. "
                           "Please use shotgun to create folders and then try again!")
         else:
+            self.log_debug("Paths to open: %s" % paths)
             # launch folder windows
             for x in paths:
-                self.launch(x)
-    
+                filesystem.open_file_browser(x)
